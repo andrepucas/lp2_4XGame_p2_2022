355cc2aefd2f257575d7c5019fd1fd6a69f3fe65
AfonsoLage-boop <afomsosantoslage@gmail.com>
2022-12-27 19:40:11 +0000

    Resources count changes according to existing base resources

--- 4XGameP2/Assets/Scripts/UI/Panels/UIPanelGameplay.cs
+++ 4XGameP2/Assets/Scripts/UI/Panels/UIPanelGameplay.cs
@@ -1,19 +1,45 @@
 using System;
+using UnityEngine;
+using TMPro;
+using System.Linq;
 
 /// <summary>
 /// Panel displayed in Gameplay UI state. Mostly contains HUD.
 /// </summary>
 public class UIPanelGameplay : UIPanel
 {
+
+    [SerializeField] private MapTilesDataSO _mapTilesData;
+    [SerializeField] private GameObject _mapResourceStat;
+    [SerializeField] private GameObject _resourcesStatLayout;
+    private MapData _mapData;
+
     /// <summary>
     /// Event raised when the the back to menu button is pressed.
     /// </summary>
     public static event Action OnRestart;
 
+    private void OnDisable()
+    {
+        
+    }
+
     /// <summary>
     /// Sets up panel.
     /// </summary>
-    public void SetupPanel() => ClosePanel();
+    public void SetupPanel()
+    {
+        foreach (String s in _mapTilesData.ResourceNames)
+        {
+            Instantiate(_mapResourceStat, _resourcesStatLayout.transform);
+
+            // Mudar o text para o count de todos os recursos desse tipo
+            
+        }
+
+        ClosePanel();
+    } 
+
 
     /// <summary>
     /// Reveals panel.
